580ffee30f4866dfc783cf2c3f1fb628d5390145
Pascal <pas111cal3@gmail.com>
2025-08-07 08:44:30 +0100

    added register feat

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+
+  
+ document.addEventListener('DOMContentLoaded', () => {
+  emailjs.init('aERXRL1Z_Z7aNsp00');
+});
+ 
+ function PopUpContainer(containerElement, open) {
+  if (containerElement) {
+    if (open === 'show') {
+      containerElement.classList.remove('hide');
+      containerElement.classList.add('show');
+    } else if (open === 'hide') {
+      containerElement.classList.remove('show');
+      containerElement.classList.add('hide');
+    } else {
+      console.error('Invalid action. Use "show" or "hide".');
+    }
+  } else {
+    console.error(`Container not found: ${containerElement}`);
+  }
+}
+
+
 const radius = 54;
 const circumference = 2 * Math.PI * radius;
 
@@ -38,7 +61,6 @@ document.querySelectorAll('.progress-container').forEach(container => {
 
 const sideBar = document.getElementById('sidebar');
 const sideBarBtn = document.getElementById('sidebar-btn');
-console.log(sideBar, sideBarBtn)
 sideBarBtn.addEventListener('click', ()=>{
  let isSidebarClosed = sideBar.getAttribute('aria-label');
     if (isSidebarClosed == 'sidebar closed'){
@@ -71,3 +93,114 @@ const services = document.querySelectorAll('.service');
     services.forEach(service => {
       observer.observe(service);
     });
+
+    function createRegisterform(){
+      return  `
+       <form  id="register-form">
+
+       <button type = "button" class = "close-register-form-btn">
+       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
+       </button>
+  <!-- Name Input -->
+  <label for="name">Name</label>
+  <input
+    id="name"
+    autocomplete="name"
+    name="name"
+    type="text"
+    placeholder="Enter your name"
+    required
+  />
+
+  <!-- Message Textarea -->
+  <label for="message">Message</label>
+  <textarea
+    id="message"
+    name="message"
+    placeholder="Enter your message"
+    rows="5"
+    required
+  ></textarea>
+
+      <!-- Subscription Plan (Radio Buttons) -->
+      <fieldset>
+        <legend>Select a Subscription Plan</legend>
+
+        <label>
+          <input type="radio" name="plan" value="standard" checked />
+          Standard Package
+        </label><br/>
+
+        <label>
+          <input type="radio" name="plan" value="premium"  />
+          Premium Package
+        </label>
+      </fieldset>
+
+      <p>
+            <a href= "#first-package" class = "see-plan-link">click</a> to see subscription plan details
+      </p>
+
+      <!-- Submit Button -->
+      <button id="register-submit-btn" type="submit">Submit</button>
+</form>
+      `
+    }
+
+
+    const popUpContainerEl = document.querySelector('.pop-up-container');
+    const registerBtn = document.querySelector('.js-register-btn');
+    registerBtn.addEventListener('click', ()=>{
+      PopUpContainer(popUpContainerEl, 'show');
+      popUpContainerEl.innerHTML = createRegisterform();
+
+      const closeFormBtn = document.querySelector('.close-register-form-btn');
+      const registerForm = document.getElementById('register-form');
+      const registerBtn = document.getElementById('register-submit-btn');
+      const seePlansLink = document.querySelector('.see-plan-link'); 
+
+      closeFormBtn.addEventListener('click',()=>{
+             PopUpContainer(popUpContainerEl, 'hide');
+              popUpContainerEl.innerHTML = '';
+      });
+
+      registerForm.addEventListener('submit', function (e) {
+          e.preventDefault();
+      
+          registerBtn.disabled = true;
+          registerBtn.textContent = 'Sending...';
+          emailjs.sendForm('service_0xakceu', 'template_g41cb2s', this, 'aERXRL1Z_Z7aNsp00')
+              .then(() => {
+                registerBtn.disabled = false;
+                registerBtn.textContent = 'Message sent!';
+                registerBtn.style.backgroundColor = 'green';
+
+                setTimeout(()=>{
+                      PopUpContainer(popUpContainerEl, 'hide');
+              popUpContainerEl.innerHTML = '';
+                }, 1000);
+          
+            })
+                .catch((error) => {
+                  registerBtn.disabled = false;
+                registerBtn.textContent = 'Message not Sent';
+                registerBtn.style.backgroundColor = 'red';
+
+                setTimeout(()=>{
+                      PopUpContainer(popUpContainerEl, 'hide');
+              popUpContainerEl.innerHTML = '';
+                }, 1000);
+                });
+            });
+
+            seePlansLink.addEventListener('click', ()=>{
+              console.log('wait 2 secs')
+              setTimeout(()=>{
+                       PopUpContainer(popUpContainerEl, 'hide');
+              popUpContainerEl.innerHTML = '';
+              }, 800);
+            });
+    });
+
+
+  
